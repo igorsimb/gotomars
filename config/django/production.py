afc709510a5d67a7538e5386c8bf4c2f41167284
igorsimb <igor.simbirtsev@gmail.com>
2025-02-24 22:13:05 +0300

    fix(production): prepare for production

--- config/django/production.py
+++ config/django/production.py
@@ -8,3 +8,23 @@ from .base import *  # noqa
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+STATIC_ROOT = "/home/c99382/gotomars.na4u.ru/www/static"
+MEDIA_ROOT = "/home/c99382/gotomars.na4u.ru/www/media"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env("DBHOST"),
+        "NAME": env("DBNAME"),
+        "USER": env("DBUSER"),
+        "PASSWORD": env("DBPASS"),
+        "PORT": "5432",
+    },
+}
+
+SECURE_HSTS_SECONDS = 60 * 60 * 24 * 365  # 1 year
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_PRELOAD = True
