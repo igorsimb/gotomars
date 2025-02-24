34065c3a996fbcbcfa74765695a1e03a8e4387c7
igorsimb <igor.simbirtsev@gmail.com>
2025-02-24 22:36:34 +0300

    fix(base): remove sqlite db from base config

--- config/django/base.py
+++ config/django/base.py
@@ -68,16 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+ASGI_APPLICATION = "config.asgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
